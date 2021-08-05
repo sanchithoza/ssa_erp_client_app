4b485e1a1426dd879118e4b76b727cb248264487
Sanchit Oza <sanchithoza@gmail.com>
2021-08-05 13:24:42 +0530

    upgrading menu

--- header.js
+++ header.js
@@ -1,3 +1,11 @@
+let url = "http://localhost:3000"
+if (!sessionStorage.length > 0) {
+    sessionStorage.clear()
+    window.location.href = "login.html"
+}
+
+let userData = JSON.parse(sessionStorage.getItem('user'))
+
 //link and css files of head section
 $("#headSection").html(`<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -17,27 +25,36 @@ $("#headSection").html(`<meta charset="UTF-8">
 <link rel="stylesheet" href="assets/css/style.min.css" />`)
 
 //left side verticle menu for navigation
-$("#left-sidebar").html(`<h5 class="brand-name">S S Agrawal </br>ERP System<a href="javascript:void(0)" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5>
+let left_sidebar_top = `<h5 class="brand-name">S S Agrawal </br>ERP System<a href="javascript:void(0)" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5>
 <ul class="nav nav-tabs">
     <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#menu-uni">Student</a></li>
 </ul>
 <div class="tab-content mt-3">
     <div class="tab-pane fade show active" id="menu-uni" role="tabpanel">
         <nav class="sidebar-nav">
-            <ul class="metismenu">
-                <li><a href="students_dashboard.html"><i class="fa fa-dashboard"></i><span>Student Dashboard</span></a></li>
-                <li><a href="students_view.html"><i class="fa fa-users"></i><span>Students</span></a></li>
-                <li><a href="institutes.html"><i class="fa fa-graduation-cap"></i><span>Institutes</span></a></li>
-                <li><a href="courses.html"><i class="fa fa-graduation-cap"></i><span>Courses</span></a></li>
-                <li><a href="class.html"><i class="fa fa-graduation-cap"></i><span>Class</span></a></li>
-                <li><a href="admin_dashboard.html"><i class="fa fa-dashboard"></i><span>Admin Dashboard</span></a></li>
-                <li><a href="admin_pending_applications.html"><i class="fa fa-dashboard"></i><span>Pending Applications</span></a></li>
-                <li><a href="admin_selected_applications.html"><i class="fa fa-dashboard"></i><span>Selected Applications</span></a></li>
-                <li><a href="admin_rejected_applications.html"><i class="fa fa-dashboard"></i><span>Rejected Applications</span></a></li>
-            </ul>
-        </nav>
-    </div>
-</div>`)
+            <ul class="metismenu">`
+let left_sidebar_bottom = `</ul>
+</nav>
+</div>
+</div>`;
+let left_sidebar_student = `<li><a href="students_dashboard.html"><i class="fa fa-dashboard"></i><span>Student Dashboard</span></a></li>
+<li><a href="students_view.html"><i class="fa fa-users"></i><span>Students</span></a></li>`;
+let left_sidebar_admin = `<li><a href="admin_dashboard.html"><i class="fa fa-dashboard"></i><span>Admin Dashboard</span></a></li>
+<li><a href="institutes.html"><i class="fa fa-graduation-cap"></i><span>Institutes</span></a></li>
+<li><a href="courses.html"><i class="fa fa-graduation-cap"></i><span>Courses</span></a></li>
+<li><a href="class.html"><i class="fa fa-graduation-cap"></i><span>Class</span></a></li>
+
+<li><a href="admin_pending_applications.html"><i class="fa fa-dashboard"></i><span>Pending Applications</span></a></li>
+<li><a href="admin_selected_applications.html"><i class="fa fa-dashboard"></i><span>Selected Applications</span></a></li>
+<li><a href="admin_rejected_applications.html"><i class="fa fa-dashboard"></i><span>Rejected Applications</span></a></li>`
+let left_sidebar = left_sidebar_top;
+if (userData.role == "student") {
+    left_sidebar += left_sidebar_student;
+} else if (userData.role == "admin") {
+    left_sidebar += left_sidebar_admin;
+}
+left_sidebar += left_sidebar_bottom;
+$("#left-sidebar").html(left_sidebar)
     //page top for user profile-logout operation
 $("#page_top").html(`<div class="container-fluid">
 <div class="page-header">
@@ -57,19 +74,13 @@ $("#page_top").html(`<div class="container-fluid">
 </div>
 </div>`);
 
-let url = "http://localhost:3000"
-if (!sessionStorage.length > 0) {
-    sessionStorage.clear()
-    window.location.href = "login.html"
-}
-
-let userData = JSON.parse(sessionStorage.getItem('user'))
 $('#user_name').html(userData.name)
 $("#log_out").click(async(e) => {
     e.preventDefault()
     await sessionStorage.clear()
     window.location.href = "login.html"
 })
+
 
 function formatDate(date) {
     var d = new Date(date),
